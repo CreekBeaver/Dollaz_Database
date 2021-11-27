fa6438ad166ebd827c5ba372594b6dbefc5b72b1
Kyle Creek <Kyle.A.Creek@gmail.com>
2021-11-26 21:52:32 -0800

    atleast I have it kinda right

--- app.py
+++ app.py
@@ -510,8 +510,8 @@ def update_aircraft_assignment():
     if request.method == "POST":
         lease_id = request.form['update'][1]
         employee_id = request.form['update'][4]
-        query = "SELECT * FROM aircraft_assignment WHERE lease_id = " + lease_id
-        query += "AND employee_id = " + employee_id + ";"
+        query = "SELECT * FROM aircraft_assignment WHERE (lease_id = " + lease_id
+        query += "AND employee_id = " + employee_id + ");"
         cursor = db.execute_query(db_connection=db_connection, query=query)
         results = cursor.fetchall()
         return render_template('update_aircraft_assignment.j2', data=results)
